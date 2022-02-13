bb4d4ec08c159cc66f53134239c5061094d20705
Cristian Rosu <cristi.epic420@gmail.com>
2022-02-13 14:53:23 +0200

    improved interaction

--- src/UI/js/index.js
+++ src/UI/js/index.js
@@ -1,36 +1,77 @@
 $(document).ready(function() {
+    /**
+     * General case: waiting to receive url and question,
+     * so we disable the last 3 buttons to force the starting
+     * order
+     */
+    $('#get-query-btn').attr('disabled', true);
+    $('#get-query-btn').css('border-style', 'solid');
+    $('#get-query-btn').css('border-color', 'gray');
+
+    $('#send-query-btn').attr('disabled', true);
+    $('#send-query-btn').css('border-style', 'solid');
+    $('#send-query-btn').css('border-color', 'gray');
+
+    $('#get-response-btn').attr('disabled', true);
+    $('#get-response-btn').css('border-style', 'solid');
+    $('#get-response-btn').css('border-color', 'gray');
+
     $('#prepare-schema-btn').click(function() {
         let question = $('#question').val();
         let apiUrl = $('#api').val();
 
-        if (!apiUrl) {
-            $('#api-label').addClass('[Error...]');
+        if (!apiUrl || !question) {
+            $('#predicted-query').text('[Error: Missing API URL or question/query...]');
+        } else {
+            /**
+             * If the "question" is in fact a graphql query, we
+             * disable the first 2 buttons and enable the last 2,
+             * it not, we proceed normally
+             */
+            if (question.includes('query')) {
+                // Enable the third one
+                $('#send-query-btn').attr('disabled', false);
+                $('#send-query-btn').css('border-style', 'none');
+            } else {
+                // Enable the second button
+                $('#get-query-btn').attr('disabled', false);
+                $('#get-query-btn').css('border-style', 'none');
+
+                $('#predicted-query').text('[Preparing...]');
+
+                axios.post('http://localhost:5000/question', {
+                    question,
+                    graphql_api_url: apiUrl
+                }).then(res => {
+                    console.log(res.data);
+                }).catch(err => {
+                    console.log(err);
+                });
+                $('#predicted-query').text('[Prepared!]');
+            }
+            // In both cases, ww disable the first button after
+            $('#prepare-schema-btn').attr('disabled', true);
+            $('#prepare-schema-btn').css('border-style', 'solid');
+            $('#prepare-schema-btn').css('border-color', 'gray');
         }
-        if (!question) {
-            $('#input-label').addClass('[Error...]');
-        }
-
-        $('#predicted-query').text('[Preparing...]');
-
-        axios.post('http://localhost:5000/question', {
-            question,
-            graphql_api_url: apiUrl
-        }).then(res => {
-            console.log(res.data);
-        }).catch(err => {
-            console.log(err);
-         });
-
-        $('#predicted-query').text('[Prepared!]');
     });
 
     $('#get-query-btn').click(function() {
         $('#predicted-query').text('[Loading...]');
 
+        // Disable this button
+        $('#get-query-btn').attr('disabled', true);
+        $('#get-query-btn').css('border-style', 'solid');
+        $('#get-query-btn').css('border-color', 'gray');
+
         axios.get('http://localhost:5000/prediction')
             .then(res => {
                 console.log(res.data);
                 $('#predicted-query').text(res.data);
+
+                // Enable the third button
+                $('#send-query-btn').attr('disabled', false);
+                $('#send-query-btn').css('border-style', 'none');
             })
             .catch(err => {
                 console.log(err);
@@ -39,30 +80,48 @@ $(document).ready(function() {
     });
 
     $('#send-query-btn').click(function() {
-        // $("#response-query").text('Marco!');
-
         let apiUrl = $('#api').val();
+        let question = $('#question').val();
         let prediction = $('#predicted-query').text();
 
-        if (!apiUrl) {
-            $('#api-label').addClass('[Error...]');
+        if (!apiUrl || !question) {
+            $('#response-query').text('[Error: Missing API URL or question/query...]');
+        } else {
+            /**
+             * If this is a question, and not a query, we proceed normally,
+             * if not we send the the query given instead of the prediction
+             */
+            $('#response-query').text('[Sending...]');
+            if (!question.includes('query')) {        
+                axios.post('http://localhost:5000/predictedQuery', {
+                    query: prediction,
+                    graphql_api_url: apiUrl
+                }).then(res => {
+                    console.log(res.data);
+                }).catch(err => {
+                    console.log(err);
+                });
+            } else {
+                axios.post('http://localhost:5000/predictedQuery', {
+                    query: question,
+                    graphql_api_url: apiUrl
+                }).then(res => {
+                    console.log(res.data);
+                }).catch(err => {
+                    console.log(err);
+                });
+            }
+    
+            $('#response-query').text('[Sent!]');
+    
+            // Disable this button and enable the last one
+            $('#send-query-btn').attr('disabled', true);
+            $('#send-query-btn').css('border-style', 'solid');
+            $('#send-query-btn').css('border-color', 'gray');
+    
+            $('#get-response-btn').attr('disabled', false);
+            $('#get-response-btn').css('border-style', 'none');
         }
-        if (!prediction || !prediction.includes('query')) {
-            $('#response-query').text('[Error...]')
-        }
-
-        $('#response-query').text('[Sending...]');
-
-        axios.post('http://localhost:5000/predictedQuery', {
-            query: prediction,
-            graphql_api_url: apiUrl
-        }).then(res => {
-            console.log(res.data);
-        }).catch(err => {
-            console.log(err);
-        });
-
-        $('#response-query').text('[Sent!]');
     });
 
     $('#get-response-btn').click(function() {
@@ -71,11 +130,22 @@ $(document).ready(function() {
         axios.get('http://localhost:5000/response')
             .then(res => {
                 console.log(res.data);
-                $('#response-query').text(res.data);
+                $('#response-query').text(JSON.stringify(res.data));
             })
             .catch(err => {
                 console.log(err);
                 $('#response-query').text('[Error!]');
             });
+
+        /**
+         * After pressing the last button, reset to original setup,
+         * so disable this button and enable the first one again
+         */
+         $('#get-response-btn').attr('disabled', true);
+         $('#get-response-btn').css('border-style', 'solid');
+         $('#get-response-btn').css('border-color', 'gray');
+ 
+         $('#prepare-schema-btn').attr('disabled', false);
+         $('#prepare-schema-btn').css('border-style', 'none');
     });
 });
